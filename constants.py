efb1a3a6f7cc9f3d3d88307ba253569dabf7b996
Bicycle Tangerine <RanZH-47>
2022-12-18 23:37:16 +0800

    Fix: flake8 max-line-length to work with black

--- constants.py
+++ constants.py
@@ -1,6 +1,11 @@
 # Amazon
 PRICE_ID = "priceblock_ourprice"
-PS4_URL = "https://www.amazon.com/-/zh/dp/B012CZ41ZA/ref=sr_1_2?__mk_zh_CN=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99&crid=TF96COU4KB1B&keywords=PS4&qid=1670838319&sprefix=p%2Caps%2C675&sr=8-2"
+PS4_URL = (
+    "https://www.amazon.com/-/"
+    + "zh/dp/B012CZ41ZA/ref=sr_1_2?__mk_zh_CN"
+    + "=%E4%BA%9A%E9%A9%AC%E9%80%8A%E7%BD%91%E7%AB%99"
+    + "&crid=TF96COU4KB1B&keywords=PS4&qid=1670838319&sprefix=p%2Caps%2C675&sr=8-2"
+)
 # Wikipedia
 WIKI_URL = "https://en.wikipedia.org/wiki/Main_Page"
 NO_SEARCH_RESULT_INFO = "There were no results matching the query."
